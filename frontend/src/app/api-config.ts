29758cb325f6405683e6f69c0a94d2840e74b1dd
= <=>
2026-06-22 03:08:14 +0530

    Update CORS with real frontend URL

--- frontend/src/app/api-config.ts
+++ frontend/src/app/api-config.ts
@@ -1,4 +1,4 @@
 import { isDevMode } from '@angular/core';
 
 // Local dev uses localhost. Production build automatically uses your live Render URL.
-export const API_BASE_URL = isDevMode() ? 'http://localhost:8000' : 'https://prisk-backend.onrender.com/'; // <-- you'll update this with your real Render URL in Part 3
+export const API_BASE_URL = isDevMode() ? 'http://localhost:8000' : 'https://prisk-backend.onrender.com'
